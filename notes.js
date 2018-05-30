14cc21c3e92279e4076a3d64d0288080f965ca33
znss1989 <znss1989@gmail.com>
2018-05-30 11:29:04 +0800

    check duplicates in title

--- notes.js
+++ notes.js
@@ -8,12 +8,14 @@ const addNote = (title, body) => {
   let notes = [];
   let notesJSON = "";
   try {
-    notesJSON = fs.readFileSync('notes-data.json');
-    notes = JSON.parse(notesJSON);
+    notesJSON = fs.readFileSync('notes-data.json'); // can be non-existing
+    notes = JSON.parse(notesJSON); // can be error coded
   } catch (err) {}
-  notes.push(note);
-  notesJSON = JSON.stringify(notes);
-  fs.writeFileSync('notes-data.json', notesJSON);
+  if (notes.every(note => note.title !== title)) {
+    notes.push(note);
+    notesJSON = JSON.stringify(notes);
+    fs.writeFileSync('notes-data.json', notesJSON);
+  }
 };
 
 const fetchNote = (title) => {
